ac4d9023c497a54921c187a3fc58e5f69369dfec
talemono <chalo.moran1701@gmail.com>
2025-04-08 00:15:49 -0300

    add analytics for app metrics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -9,5 +10,6 @@ import './i18n/i18n';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
